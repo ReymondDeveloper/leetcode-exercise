4c75b082c7f99e7ec6f40bb62c39658875592500
Reymond Peña <reymond.pena.dev@gmail.com>
2025-03-20 10:01:43 +0800

    add comment for generic

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,11 @@
 import java.util.Arrays;
 
 class Solution {
-
+    // public class ClassName <T (Any name that you want to indicate)> {
+    //  T propertiesName;
+    // }
+    // Generics class in Java
+    // Class<Datatypes> name = new Class<>();
     public int strStr(String haystack, String needle) {
         return 0 ;
     }
