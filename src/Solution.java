62edfa7782f89ee8fb5bc0e89b7c04e5dec086c7
Reymond Peña <reymond.pena.dev@gmail.com>
2025-03-22 10:38:06 +0800

    add strStr function for leetcode problem

--- src/Solution.java
+++ src/Solution.java
@@ -2,20 +2,8 @@ import java.util.Arrays;
 
 class Solution {
 
-    public static int[] plusOne(int[] nums) {
-
-        for(int i = nums.length-1; i >= 0; i--) {
-//            System.out.println(nums[i]);
-            if(nums[i] < 9) {
-                nums[i]++; // add one every digit
-                System.out.println(nums[i]);
-                return nums;
-            }
-            nums[i] = 0;
-        }
-        int[] result = new int[nums.length + 1];
-        result[0] = 1;
-        return result;
+    public static int strStr(String haystack, String needle) {
+        return 0;
     }
 
 
