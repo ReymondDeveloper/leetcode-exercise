ae362481bddf6d6d97c5396813746addca8fbe72
Reymond Peña <reymond.pena.dev@gmail.com>
2025-03-25 20:07:13 +0800

    add strStr function for leetcode problem

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String haystack = "sadbutsad", needle = "sad";
+        String haystack = "sadbutsad", needle = "sads";
 
         int ans = Solution.strStr(haystack, needle);
         System.out.println(ans);
