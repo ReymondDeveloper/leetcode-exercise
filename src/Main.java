df17e33acc69f65cfdcd9e0036840f99694c6388
Reymond Peña <reymond.pena.dev@gmail.com>
2025-04-01 21:39:04 +0800

    solve the singleNumber problem in leetcode

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,16 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
 
-
-        String haystack = "sadbutsad", needle = "sad";
-
-        int ans = Solution.strStr(haystack, needle);
+        int[] nums = {4, 1, 2, 1, 2};
+        int ans = Solution.singleNumber(nums);
         System.out.println(ans);
 
-
     }
-
-
-
-
 
 
 }
--- src/Solution.java
+++ src/Solution.java
@@ -2,9 +2,14 @@ import java.util.Arrays;
 
 class Solution {
 
-    public static int strStr(String haystack, String needle) {
-        return 0;
-    }
+   public static int singleNumber(int[] nums) {
+       // We can solved this using  XOR ^
+       int res = 0;
+       for(int n : nums ) {
+           res = res ^ n;
+       }
+       return res;
+   }
 
 
 }
