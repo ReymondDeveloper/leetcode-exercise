ea5fe54af6f4a35b4574b655e212f345dd312fd5
Reymond Peña <reymond.pena.dev@gmail.com>
2025-04-08 19:23:11 +0800

    sample code

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,22 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Choose Selection : " );
 
-        int i;
-        do{
-            System.out.println("1 Select Location");
-            System.out.println("2 List Of City");
-            System.out.println("3 Exit ");
-            System.out.print("-> ");
-            i = sc.nextInt();
-            switch (i){
-                case 1:
-                    if(Destination.map.isEmpty()) {
-                        System.out.println("Please generate cites first (Option 2) ");
-                        break;
-                    }
-                    System.out.print("Enter a number to search for : ");
-                    int num = sc.nextInt();
-                    Destination.findCitiesByNumber(num);
-                    System.out.println();
-                    break;
-                case 2:
-                    System.out.println("City Of Manila");
-                    Destination.generateCityNumber();
-                    System.out.println();
-                    break;
-                case 3:
-                    System.out.println("Exit");
-                    break;
-            }
-        }while (i != 3);
+        String[] name = {"Mark", "Paul", "Jacob", "Santos",};
+        int[] age = {33, 30, 25, 45};
 
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < name.length; i++) {
+            map.put(name[i], age[i]);
+        }
+
+        for(Map.Entry<String, Integer > details: map.entrySet()) {
+            System.out.println("Name : "  + details.getKey() +  ", Age : " + details.getValue());
+        }
 
     }
 
