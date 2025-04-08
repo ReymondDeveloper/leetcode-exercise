b046e763b0ec5ca74faca233d3a6fdfa82560e31
Reymond Peña <reymond.pena.dev@gmail.com>
2025-04-08 10:44:24 +0800

    create problem solving using map

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,41 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int[] nums = {4, 1, 2, 1, 2};
-        int ans = Solution.singleNumber(nums);
-        System.out.println(ans);
+        System.out.println("Choose Selection : " );
+
+        int i;
+        do{
+            System.out.println("1 Select Location");
+            System.out.println("2 List Of City");
+            System.out.println("3 Exit ");
+            System.out.print("-> ");
+            i = sc.nextInt();
+            switch (i){
+                case 1:
+                    if(Destination.map.isEmpty()) {
+                        System.out.println("Please generate cites first (Option 2) ");
+                        break;
+                    }
+                    System.out.print("Enter a number to search for : ");
+                    int num = sc.nextInt();
+                    Destination.findCitiesByNumber(num);
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println("City Of Manila");
+                    Destination.generateCityNumber();
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println("Exit");
+                    break;
+            }
+        }while (i != 3);
+
 
     }
 
--- src/Solution.java
+++ src/Solution.java
@@ -2,14 +2,16 @@ import java.util.Arrays;
 
 class Solution {
 
-   public static int singleNumber(int[] nums) {
-       // We can solved this using  XOR ^
-       int res = 0;
-       for(int n : nums ) {
-           res = res ^ n;
-       }
-       return res;
-   }
+
+
+//   public static int singleNumber(int[] nums) {
+//       // We can solved this using  XOR ^
+//       int res = 0;
+//       for(int n : nums ) {
+//           res = res ^ n;
+//       }
+//       return res;
+//   }
 
 
 }
