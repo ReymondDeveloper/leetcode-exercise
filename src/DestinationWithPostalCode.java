b08828926a827f6b15f4cef7db2e13a27607c29d
Reymond Peña <reymond.pena.dev@gmail.com>
2025-04-13 22:49:41 +0800

    create new problem solving in leetcode

--- src/DestinationWithPostalCode.java
+++ src/DestinationWithPostalCode.java
@@ -1,3 +1,55 @@
+import java.awt.List;
+import java.util.*;
+
 public class DestinationWithPostalCode {
+
+
+
+    static String[] destination = {"Palawaan","Romblon","Boracay"};
+    static Map<String, Integer> map = new HashMap<>();
+
+    public static void GeneratePostalCode() {
+        Set<Integer> unique = new HashSet<>();
+        Random random = new Random();
+        map.clear();
+        for(String des : destination) {
+            int con;
+            do{
+                con = random.nextInt(1,100);
+            }while (unique.contains(con));
+
+            unique.add(con);
+            map.put(des, con);
+        }
+        GetAll();
+    }
+
+
+    public static void GetAll() {
+        for(Map.Entry<String,Integer> mapUnique: map.entrySet()){
+            System.out.println("City : " + mapUnique.getKey() + " Postal Code : " + mapUnique.getValue());
+        }
+        System.out.println();
+    }
+
+    public static void GetPostalCode(int postalCode) {
+        boolean isTrue = false;
+
+        for(Map.Entry<String, Integer > postalCodeUnique : map.entrySet()){
+            if(postalCodeUnique.getValue() == postalCode){
+                System.out.println(" City : " + postalCodeUnique.getKey());
+                isTrue = true;
+            }
+        }
+
+        if(!isTrue) {
+            System.out.println("Postal Code Not Found");
+        }
+
+        System.out.println();
+    }
+
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,33 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        String[] citys = {"Quezon","Pasig","Makati","Pasay"};
+        int num ;
 
+        do{
+
+            System.out.println("Choose Your Destination");
+            System.out.println(" 1 All Data");
+            System.out.println(" 2 Search Postal Code");
+            System.out.println(" 3 Exit");
+            num = sc.nextInt();
+
+            switch (num) {
+                case 1 :
+                    System.out.println();
+                    DestinationWithPostalCode.GeneratePostalCode();
+                    break;
+                case 2 :
+                    System.out.println();
+                    System.out.println(" Input Postal Code ");
+                    num = sc.nextInt();
+                    DestinationWithPostalCode.GetPostalCode(num);
+                    System.out.println();
+                    break;
+                case 3 :
+                    System.out.println("Thank You For Using My Application Bye !!");
+                    break;
+            }
+        }while (true);
 
     }
 
