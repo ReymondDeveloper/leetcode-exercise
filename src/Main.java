04f2a078a32a351a70412ff0611963face64afb5
Reymond Peña <reymond.pena.dev@gmail.com>
2025-04-15 11:51:33 +0800

    create new problem solving in leetcode

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int count = 0;
-        int cand = 0;
 
-        int[] nums = {2,1,1,1,2};
-
-
-        for(int i = 0; i < nums.length; i++) {
-            if(count == 0) {
-                cand = nums[i];
-            }
-
-            if(cand==nums[i]){
-                count++;
-                System.out.println("Add " + count );
-            } else {
-                count--;
-                System.out.println("Minus " + count );
-            }
-
-        }
-
-        System.out.println(cand);
     }
 
 }
--- src/Solution.java
+++ src/Solution.java
@@ -14,11 +14,23 @@ class Solution {
 //       return res;
 //   }
 
-    public static int MajorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-            return 0;
+    public static int majorityElement(int[] nums) {
+        // It works for two numbers display
+        int count = 0;
+        int containerOfNums = 0;
+        for(int i = 0; i < nums.length; i++) {
 
+            if(count == 0) {
+                containerOfNums = nums[i];
+            }
 
+            if(containerOfNums == nums[i]){
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return containerOfNums;
     }
 
 
